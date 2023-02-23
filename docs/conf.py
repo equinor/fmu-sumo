bae26447730b7b30911e9bf09d05bd3162156446
Ådne Jacobsen <adnj@equinor.com>
2023-02-23 11:34:56 +0100

    test

--- docs/conf.py
+++ docs/conf.py
@@ -48,14 +48,6 @@ togglebutton_hint = "Expand"
 autodoc_mock_imports = ["ert", "ert_shared", "sumo", "xtgeo", "pandas"]
 autodoc_default_flags = ["members", "show-inheritance", "inherited-members"]
 
-os.environ["SPHINX_APIDOC_OPTIONS"] = json.dumps(
-    [
-        "members",
-        "show-inheritance",
-        "inherited-members",
-    ]
-)
-
 apidoc_module_dir = "../src/fmu"
 apidoc_output_dir = "apiref"
 apidoc_excluded_paths = ["version.py", "hook_implementations"]
