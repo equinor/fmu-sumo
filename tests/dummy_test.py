11de02589be7077b652ad68d95395572c03ab6c6
Roy Willy Haug <rowh@equinor.com>
2023-03-21 14:35:01 +0100

    Debug gh actions tests

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -6,9 +6,9 @@ def test_uppercase():
 def test_sumo_perms():
     sumo = Explorer("dev", interactive=False)
     print("Before perms")
-    perms = sumo.get_permissions()
-    print("After perms")
-    print(perms)
+    # perms = sumo.get_permissions()
+    # print("After perms")
+    # print(perms)
 
 def test_sumo_fail():
     raise Exception("dummyexc")
