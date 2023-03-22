e5e3a0d512a6b335cedf5ff98991c31efef4552c
Roy Willy Haug <rowh@equinor.com>
2023-03-22 08:14:29 +0100

    Debug tests

---
+++ tests/dummy_test.py
@@ -0,0 +1,14 @@
+from fmu.sumo.explorer import Explorer
+
+def test_uppercase():
+    assert "loud noises".upper() == "LOUD NOISES"
+
+def test_sumo_perms():
+    sumo = Explorer("dev", interactive=False)
+    print("Before perms")
+    perms = sumo.get_permissions()
+    print("After perms")
+    print(perms)
+
+# def test_sumo_fail():
+#     raise Exception("dummyexc")
