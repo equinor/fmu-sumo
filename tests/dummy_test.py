62eaa3d26b44419376f67bb35f20af0ddc9cb541
Roy Willy Haug <rowh@equinor.com>
2023-03-22 08:24:02 +0100

    Debug tests

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -1,7 +1,12 @@
+import os
 from fmu.sumo.explorer import Explorer
 
 def test_uppercase():
     assert "loud noises".upper() == "LOUD NOISES"
+
+def test_env():
+    token = os.getenv('ACCESS_TOKEN')
+    assert len(token) > 20
 
 def test_sumo_perms():
     sumo = Explorer("dev", interactive=False)
