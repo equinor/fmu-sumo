083ba6e8b8267dd3883133ed97690ffce015348b
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-08-21 15:40:48 +0200

    Make Submodules tuple and dictionary global

--- src/fmu/sumo/utilities/ecl2csv.py
+++ src/fmu/sumo/utilities/ecl2csv.py
@@ -2,13 +2,12 @@
 from pathlib import Path
 import logging
 import importlib
+import argparse
 from inspect import signature, Parameter
 import pandas as pd
 import ecl2df
 from fmu.config.utilities import yaml_load
 from fmu.dataio import ExportData
-
-UNWANTEDS = ["inferdims", "__init__", "version", "ecl2csv", "common"]
 
 
 def define_submodules():
@@ -17,14 +16,11 @@ def define_submodules():
     Returns:
         list: list of submodules
     """
+
+    logger = logging.getLogger(__file__ + "define_submodules")
     package_path = Path(ecl2df.__file__).parent
 
-    # submodules = [
-    #     str(submod.name).replace(".py", "")
-    #     for submod in list(submod_path.glob("*.py"))
-    # ]
-    logger = logging.getLogger(__file__ + "define_submodules")
-    submodules = []
+    submodules = {}
     for submod_path in package_path.glob("*.py"):
         submod = str(submod_path.name.replace(".py", ""))
         try:
@@ -32,30 +28,14 @@ def define_submodules():
         except AttributeError:
             logger.debug("No df function in %s", submod_path)
             continue
-        sig_items = signature(func).parameters.items()
-        print(sig_items)
+        submodules[submod] = tuple(signature(func).parameters.keys())
+        logger.debug("Assigning %s to %s", submodules[submod], submod)
 
-        logger.debug(
-            "%s has the following parameters %s", func.__name__, sig_items
-        )
-        submodules.append(submod)
-
-    # submodules = [
-    #     "summary",
-    #     "rft",
-    #     "satfunc",
-    #     "grid",
-    #     "faults",
-    #     "pillars",
-    #     "pvt",
-    #     "trans",
-    #     "nnc",
-    #     "compdat",
-    #     "gruptree",
-    #     "equil",
-    # ]
     logger.debug(submodules)
-    return submodules
+    return tuple(submodules.keys()), submodules
+
+
+SUBMODULES, SUBMOD_DICT = define_submodules()
 
 
 def give_name(eclpath: str) -> str:
@@ -74,24 +54,31 @@ def give_name(eclpath: str) -> str:
     return base_name
 
 
-def get_dataframe(eclpath: str, submod: str) -> pd.DataFrame:
+def get_dataframe(eclpath: str, submod: str, **kwargs) -> pd.DataFrame:
     """Fetch dataframe from eclipse results
 
     Args:
         eclpath (str): the path to the eclipse datafile
         submod (str): the name of the submodule to extract with
+        kwargs (dict): other options
 
     Returns:
         pd.DataFrame: the extracted data
     """
     extract_df = importlib.import_module("ecl2df." + submod).df
-    return extract_df(ecl2df.EclFiles(eclpath))
+    right_kwargs = {
+        key: value
+        for key, value in kwargs.items()
+        if key in SUBMOD_DICT[submod]
+    }
+    return extract_df(ecl2df.EclFiles(eclpath), **right_kwargs)
 
 
 def export_csv(
     eclpath: str,
     submod: str,
     global_variables_file="fmuconfig/output/global_variables.yml",
+    **kwargs,
 ) -> str:
     """Export csv file with specified datatype
 
@@ -102,7 +89,7 @@ def export_csv(
     Returns:
         str: path of export
     """
-    frame = get_dataframe(eclpath, submod)
+    frame = get_dataframe(eclpath, submod, **kwargs)
     logger = logging.getLogger(__file__ + "export_csv")
     logger.debug("Reading global variables from %s", global_variables_file)
     cfg = yaml_load(global_variables_file)
@@ -112,3 +99,51 @@ def export_csv(
         tagname=submod,
     )
     return exp.export(frame)
+
+
+def parse_args():
+    """Parse arguments"""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=(
+            "ecl2csv ("
+            # + __version__
+            + ") is a modified command line frontend to ecl2df. "
+        ),
+    )
+    # parser.add_argument(
+    #     "--datafile_path",
+    #     type=str,
+    #     help="path to eclipse datafile for run",
+    # )
+    subparsers = parser.add_subparsers(
+        required=True,
+        dest="subcommand",
+        parser_class=argparse.ArgumentParser,
+    )
+    for submod in SUBMODULES:
+        subparser = subparsers.add_parser(submod)
+        importlib.import_module("ecl2df." + submod).fill_parser(subparser)
+    parser.add_argument(
+        "--config_path",
+        type=str,
+        help="fmu config file to enable exporting with metadata",
+        default="fmuconfig/output/global_variables.yml",
+    )
+    args = parser.parse_args()
+    return vars(args)
+
+
+def export_csv_from_command_line():
+    """Execute from command line"""
+    submod = "summary"
+    args = parse_args()
+    export_csv(
+        args.eclpath,
+        submod,
+        global_variables_file="fmuconfig/output/global_variables.yml",
+    )
+
+
+def main():
+    parse_args()
--- tests/test_utility_ec2csv.py
+++ tests/test_utility_ec2csv.py
@@ -2,25 +2,31 @@
 from pathlib import Path
 import pandas as pd
 import pytest
+from fmu.sumo.utilities.ecl2csv import SUBMODULES
+from fmu.sumo.utilities.ecl2csv import SUBMOD_DICT
 from fmu.sumo.utilities.ecl2csv import define_submodules
 from fmu.sumo.utilities.ecl2csv import get_dataframe
 from fmu.sumo.utilities.ecl2csv import export_csv
+from fmu.sumo.utilities.ecl2csv import parse_args
+
 
 REEK_DATA = Path(__file__).parent / "data/reek/eclipse/model/"
 REEK_DATA_FILE = REEK_DATA / "2_R001_REEK-0.DATA"
 
 
-def test_submodules_list():
+def test_submodules_dict():
     """Test generation of submodule list"""
-    submods = define_submodules()
-    assert isinstance(submods, list)
-    for submod in submods:
-        assert isinstance(submod, str)
-        assert "/" not in submod
+    sublist, submods = define_submodules()
+    assert isinstance(sublist, tuple)
+    assert isinstance(submods, dict)
+    for submod_name, submod_input in submods.items():
+        assert isinstance(submod_name, str)
+        assert "/" not in submod_name
+        assert isinstance(submod_input, tuple)
     print(submods)
 
 
-@pytest.mark.parametrize("submod", define_submodules())
+@pytest.mark.parametrize("submod", SUBMODULES)
 def test_get_dataframe(submod):
     """Test fetching of dataframe"""
     frame = get_dataframe(REEK_DATA_FILE, submod)
@@ -37,3 +43,20 @@ def test_export_csv(submod="summary"):
         ),
         str,
     )
+
+
+def test_parse_args(tmp_path, mocker):
+    mocker.path(
+        "sys.argv",
+        [
+            "ecl2csv",
+            Path(__file__).parent
+            / "data/reek/fmuconfig/output/global_variables.yml",
+            "summary",
+        ],
+    )
+    parse_args()
+
+
+if __name__ == "__main__":
+    test_parse_args()
