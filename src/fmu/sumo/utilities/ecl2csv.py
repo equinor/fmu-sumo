14786d35276191a8f6b67e7af3b5e053592b3e5c
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-08-23 15:23:49 +0200

    Commit first working version

--- src/fmu/sumo/utilities/ecl2csv.py
+++ src/fmu/sumo/utilities/ecl2csv.py
@@ -1,16 +1,18 @@
 """Export with metadata"""
+from typing import Tuple
 from pathlib import Path
 import logging
 import importlib
 import argparse
-from inspect import signature, Parameter
+from inspect import signature
 import pandas as pd
 import ecl2df
+from pyarrow import Table
 from fmu.config.utilities import yaml_load
 from fmu.dataio import ExportData
 
 
-def define_submodules():
+def _define_submodules():  #  -> Tuple(tuple, dict):
     """Fetch all submodules
 
     Returns:
@@ -28,54 +30,140 @@ def define_submodules():
         except AttributeError:
             logger.debug("No df function in %s", submod_path)
             continue
-        submodules[submod] = tuple(signature(func).parameters.keys())
+        submodules[submod] = {"extract": func}
+        submodules[submod]["options"] = tuple(
+            name
+            for name in signature(func).parameters.keys()
+            if name not in ["deck", "eclfiles"]
+        )
+        submodules[submod]["doc"] = func.__doc__
+        try:
+            submodules[submod]["arrow_convertor"] = importlib.import_module(
+                "ecl2df." + submod
+            )._df2pyarrow
+        except AttributeError:
+            logger.info(
+                "No premade function for converting to arrow in %s",
+                submod_path,
+            )
         logger.debug("Assigning %s to %s", submodules[submod], submod)
 
-    logger.debug(submodules)
+    logger.debug(
+        "Returning the submodule names as a list: %s ", submodules.keys()
+    )
+    logger.debug(
+        "Returning the submodules extra args as a dictionary: %s ", submodules
+    )
+
     return tuple(submodules.keys()), submodules
 
 
-SUBMODULES, SUBMOD_DICT = define_submodules()
+SUBMODULES, SUBMOD_DICT = _define_submodules()
 
 
-def give_name(eclpath: str) -> str:
+def give_name(datafile_path: str) -> str:
     """Return name to assign in metadata
 
     Args:
-        eclpath (str): path to the eclipse datafile
+        datafile_path (str): path to the simulator datafile
 
     Returns:
         str: derived name
     """
-    eclpath_posix = Path(eclpath)
-    base_name = eclpath_posix.name.replace(eclpath_posix.suffix, "")
+    datafile_path_posix = Path(datafile_path)
+    base_name = datafile_path_posix.name.replace(
+        datafile_path_posix.suffix, ""
+    )
     while base_name[-1].isdigit() or base_name.endswith("-"):
         base_name = base_name[:-1]
     return base_name
 
 
-def get_dataframe(eclpath: str, submod: str, **kwargs) -> pd.DataFrame:
-    """Fetch dataframe from eclipse results
+def convert_to_arrow(frame):
+    """Convert pd.DataFrame to arrow
 
     Args:
-        eclpath (str): the path to the eclipse datafile
+        frame (pd.DataFrame): the frame to convert
+
+    Returns:
+        pa.Table: the converted dataframe
+    """
+    table = Table.from_pandas(frame)
+    return table
+
+
+def get_dataframe(
+    datafile_path: str, submod: str, print_help=False, **kwargs
+) -> pd.DataFrame:
+    """Fetch dataframe from simulator results
+
+    Args:
+        datafile_path (str): the path to the simulator datafile
         submod (str): the name of the submodule to extract with
         kwargs (dict): other options
 
     Returns:
         pd.DataFrame: the extracted data
     """
-    extract_df = importlib.import_module("ecl2df." + submod).df
-    right_kwargs = {
-        key: value
-        for key, value in kwargs.items()
-        if key in SUBMOD_DICT[submod]
-    }
-    return extract_df(ecl2df.EclFiles(eclpath), **right_kwargs)
+    logger = logging.getLogger(__file__ + ".get_dataframe")
+    extract_df = SUBMOD_DICT[submod]["extract"]
+    arrow = kwargs.get("arrow", False)
+    frame = None
+    if print_help:
+        print(SUBMOD_DICT[submod]["doc"])
+    else:
+        right_kwargs = {
+            key: value
+            for key, value in kwargs.items()
+            if key in SUBMOD_DICT[submod]
+        }
+        logger.debug("Exporting with arguments %s", right_kwargs)
+        try:
+            frame = extract_df(ecl2df.EclFiles(datafile_path), **right_kwargs)
+            if arrow:
+                try:
+                    frame = kwargs["arrow_convertor"](frame)
+                except KeyError:
+                    logger.debug("No arrow convertor defined for %s", submod)
+                    frame = convert_to_arrow(frame)
+
+        except TypeError:
+            logger.warning(
+                "Couldn't produce results!! Most likely something with you call"
+            )
+        except FileNotFoundError:
+            logger.warning(
+                "Cannot produce results!!, most likely no results present"
+            )
+    return frame
+
+
+def check_options(submod, key_args):
+    """Check keyword args against possible optiopns
+
+    Args:
+        submod (str): the submodule to use from ecl2df
+        key_args (dict): the keyword arguments supplied
+
+    Raises:
+        KeyError: if key(s) in key_args not available for submodule
+    """
+    unknowns = [
+        key
+        for key in key_args.keys()
+        if key not in SUBMOD_DICT[submod]["options"]
+    ]
+    if len(unknowns) > 0:
+        raise KeyError(
+            (
+                f"these unknown options are included in call {unknowns}\n"
+                + f"please read the docs: \n {SUBMOD_DICT[submod]['doc']}"
+            )
+        )
 
 
 def export_csv(
-    eclpath: str,
+    datafile_path: str,
     submod: str,
     global_variables_file="fmuconfig/output/global_variables.yml",
     **kwargs,
@@ -83,67 +171,169 @@ def export_csv(
     """Export csv file with specified datatype
 
     Args:
-        eclpath (str): the path to the eclipse datafile
+        datafile_path (str): the path to the simulator datafile
         submod (str): the name of the submodule to extract with
 
     Returns:
         str: path of export
     """
-    frame = get_dataframe(eclpath, submod, **kwargs)
-    logger = logging.getLogger(__file__ + "export_csv")
-    logger.debug("Reading global variables from %s", global_variables_file)
-    cfg = yaml_load(global_variables_file)
-    exp = ExportData(
-        config=cfg,
-        name=give_name(eclpath),
-        tagname=submod,
-    )
-    return exp.export(frame)
+    logger = logging.getLogger(__file__ + ".export_csv")
+    # check_options(submod, kwargs)
+    frame = get_dataframe(datafile_path, submod, **kwargs)
+    if frame is not None:
+        logger.debug("Reading global variables from %s", global_variables_file)
+        cfg = yaml_load(global_variables_file)
+        exp = ExportData(
+            config=cfg,
+            name=give_name(datafile_path),
+            tagname=submod,
+        )
+        exp_path = exp.export(frame)
+    else:
+        exp_path = "Nothing produces"
+    return exp_path
 
 
-def parse_args():
-    """Parse arguments"""
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description=(
-            "ecl2csv ("
-            # + __version__
-            + ") is a modified command line frontend to ecl2df. "
-        ),
-    )
-    # parser.add_argument(
-    #     "--datafile_path",
-    #     type=str,
-    #     help="path to eclipse datafile for run",
-    # )
-    subparsers = parser.add_subparsers(
-        required=True,
-        dest="subcommand",
-        parser_class=argparse.ArgumentParser,
-    )
-    for submod in SUBMODULES:
-        subparser = subparsers.add_parser(submod)
-        importlib.import_module("ecl2df." + submod).fill_parser(subparser)
-    parser.add_argument(
-        "--config_path",
-        type=str,
-        help="fmu config file to enable exporting with metadata",
-        default="fmuconfig/output/global_variables.yml",
-    )
-    args = parser.parse_args()
-    return vars(args)
+def read_config(config):
+    """Read config settings
+
+    Args:
+        config (dict): the settings for export of simulator results
+
+    Returns:
+        tuple: datafiles as list, submodules to use as list, and options as kwargs
+    """
+    # datafile can be read as list, or string which can be either folder or filepath
+    logger = logging.getLogger(__file__ + ".read_config")
+    if isinstance(config, bool):
+        config = {}
+    datafile = config.get("datafile", "eclipse/model/")
+    if isinstance(datafile, str):
+        datafile_posix = Path(datafile)
+        if datafile_posix.is_dir():
+            datafiles = list(datafile_posix.glob("*.DATA"))
+        else:
+            datafiles = [datafile]
+    else:
+        datafiles = datafile
+    try:
+        submods = config["datatypes"]
+    except KeyError:
+        submods = SUBMODULES
+    try:
+        options = config["options"]
+    except KeyError:
+        logger.info("No special options selected")
+        options = {}
+    options["arrow"] = options.get("arrow", False)
+    return datafiles, submods, options
 
 
-def export_csv_from_command_line():
-    """Execute from command line"""
-    submod = "summary"
-    args = parse_args()
-    export_csv(
-        args.eclpath,
-        submod,
-        global_variables_file="fmuconfig/output/global_variables.yml",
-    )
+def export_with_config(config_path):
+    """Export several datatypes with yaml config file
+
+    Args:
+        config_path (str): path to existing yaml file
+    """
+    logger = logging.getLogger(__file__ + ".export_w_config")
+    config = yaml_load(config_path)
+    try:
+        sim_specifics = config["ecl2csv"]
+        datafiles, submods, options = read_config(sim_specifics)
+        for datafile in datafiles:
+            for submod in submods:
+                export_csv(
+                    datafile,
+                    submod,
+                    global_variables_file=config_path,
+                    **options,
+                )
+    except KeyError:
+        logger.warning(
+            "No export from reservoir simulator, forgot to include ecl2csv in config?"
+        )
 
 
-def main():
-    parse_args()
+# def filter_args_w_options(args):
+#     """Filter the command line arguments to fit to function
+
+#     Args:
+#         args (argparse.NameSpace): the arguments are namespace
+
+#     Returns:
+#         dict: the filtered arguments
+#     """
+#     logger = logging.getLogger(__file__ + ".filter_args_w_options")
+#     export_arrow = False
+#     try:
+#         export_arrow = args.arrow = True
+#     except AttributeError:
+#         logger.info("No option for export to arrow in %s", args.subcommand)
+#     key_args = {
+#         key: value
+#         for key, value in vars(args).items()
+#         if key in SUBMOD_DICT[args.subcommand]
+#     }
+#     if export_arrow:
+#         key_args["arrow_convertor"] = SUBMOD_DICT[args.subcommand][
+#             "arrow_convertor"
+#         ]
+#     logger.debug("Arguments filtered: %s", key_args)
+#     return key_args
+
+
+# def parse_args():
+#     """Parse arguments"""
+#     parser = argparse.ArgumentParser(
+#         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+#         description=(
+#             "ecl2csv ("
+#             # + __version__
+#             + ") is a modified command line frontend to ecl2df. "
+#         ),
+#     )
+#     # parser.add_argument(
+#     #     "--datafile_path",
+#     #     type=str,
+#     #     help="path to simulator datafile for run",
+#     # )
+#     subparsers = parser.add_subparsers(
+#         required=True,
+#         dest="subcommand",
+#         parser_class=argparse.ArgumentParser,
+#     )
+#     for submod in SUBMODULES:
+#         subparser = subparsers.add_parser(submod)
+#         importlib.import_module("ecl2df." + submod).fill_parser(subparser)
+#     parser.add_argument(
+#         "--config_path",
+#         type=str,
+#         help="fmu config file to enable exporting with metadata",
+#         default="fmuconfig/output/global_variables.yml",
+#     )
+#     args = filter_args_w_options(parser.parse_args())
+
+#     return args
+
+
+# def export_csv_with_args(
+#     args,
+# ):
+#     """Export datatypes from simulator from command line
+
+#     Args:
+#         args (argparse.NameSpace): Input arguments
+#     """
+#     logger = logging.getLogger(__file__ + ".export_csv_with_args")
+#     export_csv(
+#         args.DATAFILE, args.subcommand, global_variables_file, **key_args
+#     )
+
+
+# def main():
+#     # export_csv_with_args(parse_args())
+#     print(parse_args())
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_utility_ec2csv.py
+++ tests/test_utility_ec2csv.py
@@ -1,61 +1,233 @@
 """Test utility ecl2csv"""
+import os
 from pathlib import Path
 import pandas as pd
+import pyarrow as pa
 import pytest
-from fmu.sumo.utilities.ecl2csv import SUBMODULES
-from fmu.sumo.utilities.ecl2csv import SUBMOD_DICT
-from fmu.sumo.utilities.ecl2csv import define_submodules
-from fmu.sumo.utilities.ecl2csv import get_dataframe
-from fmu.sumo.utilities.ecl2csv import export_csv
-from fmu.sumo.utilities.ecl2csv import parse_args
+import fmu.sumo.utilities.ecl2csv as sumo_ecl2csv
 
-
-REEK_DATA = Path(__file__).parent / "data/reek/eclipse/model/"
-REEK_DATA_FILE = REEK_DATA / "2_R001_REEK-0.DATA"
+REEK_ROOT = Path(__file__).parent / "data/reek"
+REEK_BASE = "2_R001_REEK"
+REEK_ECL_MODEL = REEK_ROOT / "eclipse/model/"
+REEK_DATA_FILE = REEK_ECL_MODEL / f"{REEK_BASE}-0.DATA"
+CONFIG_OUT_PATH = REEK_ROOT / "fmuconfig/output/"
+CONFIG_PATH = CONFIG_OUT_PATH / "global_variables.yml"
 
 
 def test_submodules_dict():
     """Test generation of submodule list"""
-    sublist, submods = define_submodules()
+    sublist, submods = sumo_ecl2csv._define_submodules()
+    print(submods)
     assert isinstance(sublist, tuple)
     assert isinstance(submods, dict)
-    for submod_name, submod_input in submods.items():
+    for submod_name, submod_dict in submods.items():
+        print(submod_name)
+        print(submod_dict)
         assert isinstance(submod_name, str)
-        assert "/" not in submod_name
-        assert isinstance(submod_input, tuple)
-    print(submods)
+        assert (
+            "/" not in submod_name
+        ), f"Left part of folder path for {submod_name}"
+        assert isinstance(submod_dict, dict), f"{submod_name} has no subdict"
+        assert (
+            "options" in submod_dict.keys()
+        ), f"{submod_name} does not have any options"
+
+        assert isinstance(
+            submod_dict["options"], tuple
+        ), f"options for {submod_name} not tuple"
 
 
-@pytest.mark.parametrize("submod", SUBMODULES)
+@pytest.mark.parametrize(
+    "submod",
+    (name for name in sumo_ecl2csv.SUBMODULES if name != "wellcompletiondata"),
+)
+# Skipping wellcompletion data, since this needs zonemap, which none of the others do
 def test_get_dataframe(submod):
     """Test fetching of dataframe"""
-    frame = get_dataframe(REEK_DATA_FILE, submod)
-    assert isinstance(frame, pd.DataFrame)
-
-
-def test_export_csv(submod="summary"):
-    """Test writing of csv file"""
+    extras = {}
+    if submod == "wellcompletiondata":
+        extras["zonemap"] = "data/reek/zones.lyr"
+    frame = sumo_ecl2csv.get_dataframe(REEK_DATA_FILE, submod)
     assert isinstance(
-        export_csv(
-            REEK_DATA_FILE,
-            submod,
-            "data/reek/fmuconfig/output/global_variables.yml",
-        ),
+        frame, pd.DataFrame
+    ), f"Call for get_dataframe should produce dataframe, but produces {type(frame)}"
+    frame = sumo_ecl2csv.get_dataframe(REEK_DATA_FILE, submod, arrow=True)
+    assert isinstance(
+        frame, pa.Table
+    ), f"Call for get_dataframe with arrow=True should produce pa.Table, but produces {type(frame)}"
+
+
+@pytest.mark.parametrize(
+    "submod",
+    (name for name in sumo_ecl2csv.SUBMODULES if name != "wellcompletiondata"),
+)
+def test_export_csv(tmp_path, submod):
+    """Test writing of csv file"""
+    os.chdir(tmp_path)
+    export_path = (
+        tmp_path / f"share/results/tables/{REEK_BASE}--{submod}.csv".lower()
+    )
+    meta_path = export_path.parent / f".{export_path.name}.yml"
+    actual_path = sumo_ecl2csv.export_csv(
+        REEK_DATA_FILE,
+        submod,
+        CONFIG_PATH,
+    )
+    print(actual_path)
+    assert isinstance(
+        actual_path,
         str,
+    ), "No string returned for path"
+    assert export_path.exists(), f"No export of data to {export_path}"
+    assert meta_path.exists(), f"No export of metadata to {meta_path}"
+
+
+def test_export_csv_w_options(tmp_path, submod="summary"):
+    """Test writing of csv file"""
+    os.chdir(tmp_path)
+    export_path = (
+        tmp_path / f"share/results/tables/{REEK_BASE}--{submod}.csv".lower()
+    )
+    key_args = {
+        "time_index": "daily",
+        "start_date": "2002-01-02",
+        "end_date": "2003-01-02",
+    }
+
+    meta_path = export_path.parent / f".{export_path.name}.yml"
+    actual_path = sumo_ecl2csv.export_csv(
+        REEK_DATA_FILE, submod, CONFIG_PATH, **key_args
+    )
+    print(actual_path)
+    assert isinstance(
+        actual_path,
+        str,
+    ), "No string returned for path"
+    assert export_path.exists(), f"No export of data to {export_path}"
+    assert meta_path.exists(), f"No export of metadata to {meta_path}"
+
+
+# Extra checks to be used with parametrize below
+CHECK_DICT = {
+    "global_variables_w_eclpath.yml": {
+        "nrdatafile": 1,
+        "nrsubmods": 16,
+        "nroptions": 1,
+        "arrow": False,
+    },
+    "global_variables_w_eclpath_and_extras.yml": {
+        "nrdatafile": 1,
+        "nrsubmods": 3,
+        "nroptions": 4,
+        "arrow": True,
+    },
+    "global_variables.yml": {
+        "nrdatafile": 2,
+        "nrsubmods": 16,
+        "nroptions": 1,
+        "arrow": False,
+    },
+}
+
+
+def _assert_right_len(checks, key, to_messure, name):
+    """Assert length when reading config
+
+    Args:
+        checks (dict): the answers
+        key (str): the answer to check
+        to_messure (list): the generated answer
+        name (str): name of the file to check against
+    """
+    # Helper for test_read_config
+    right_len = checks[key]
+    actual_len = len(to_messure)
+    assert (
+        actual_len == right_len
+    ), f"For {name}-{key} actual length is {actual_len}, but should be {right_len}"
+
+
+@pytest.mark.parametrize("config_path", CONFIG_OUT_PATH.glob("*.yml"))
+def test_read_config(config_path):
+    """Test reading of config file via read_config function"""
+    os.chdir(REEK_ROOT)
+    print(config_path)
+    config = sumo_ecl2csv.yaml_load(config_path)["ecl2csv"]
+    assert isinstance(config, (dict, bool))
+    dfiles, submods, opts = sumo_ecl2csv.read_config(config)
+    name = config_path.name
+    checks = CHECK_DICT[name]
+    print(config)
+    print(dfiles)
+    print(submods)
+    print(opts)
+    _assert_right_len(checks, "nrdatafile", dfiles, name)
+    _assert_right_len(checks, "nrsubmods", submods, name)
+    _assert_right_len(checks, "nroptions", opts, name)
+
+    assert (
+        opts["arrow"] == checks["arrow"]
+    ), f"Wrong choice for arrow for {name}"
+
+
+@pytest.mark.parametrize("config_path", CONFIG_OUT_PATH.glob("*.yml"))
+def test_export_w_config(tmp_path, config_path):
+    """Test function export with config"""
+    os.chdir(tmp_path)
+    # The line below is needed for test to work when def of datafile not in
+    # config
+    sim_path = tmp_path / "eclipse"
+    conf_path = tmp_path / "fmuconfig/output/"
+    sim_path.mkdir(parents=True)
+    (sim_path / "model").symlink_to(REEK_ECL_MODEL)
+    conf_path.mkdir(parents=True)
+    (conf_path / config_path.name).symlink_to(config_path)
+
+    sumo_ecl2csv.export_with_config(config_path)
+
+
+def test_parse_args(mocker, submod):
+    """Test parse args
+
+    Args:
+        mocker (pytest.fixture): to mock command line like
+        submod (str): name of submodule
+    """
+    config_path = str(
+        Path(__file__).parent
+        / "data/reek/fmuconfig/output/global_variables.yml",
     )
 
+    reek_datafile_str = str(REEK_DATA_FILE)
+    commands = [config_path, submod, reek_datafile_str]
 
-def test_parse_args(tmp_path, mocker):
-    mocker.path(
+    print(commands)
+    mocker.patch(
         "sys.argv",
-        [
-            "ecl2csv",
-            Path(__file__).parent
-            / "data/reek/fmuconfig/output/global_variables.yml",
-            "summary",
-        ],
+        commands,
     )
-    parse_args()
+    args = sumo_ecl2csv.parse_args()
+    assert isinstance(args, dict), "Args not converted to dict"
+    assert (
+        args["subcommand"] == submod
+    ), f"For {submod} subcommand is set to {args['subcommand']}"
+    assert (
+        args["DATAFILE"] == reek_datafile_str
+    ), f"For {submod} datafile is {args['DATAFILE']}"
+    options = sumo_ecl2csv.SUBMOD_DICT[submod]
+    arg_keys = args.keys()
+    print(options)
+    print(arg_keys)
+    unknowns = [arg_key for arg_key in arg_keys if arg_key not in options]
+    assert (
+        len(unknowns) == 0
+    ), f"for {submod} these unknown arguments were passed {unknowns}"
+
+    assert all([arg in options for arg in arg_keys]), "Not all passed"
+
+
+def test_export_from_commmandline(tmp_path):
+    tmpcsvfile = tmp_path / "2_R001_REEK--summary.csv"
 
 
 if __name__ == "__main__":
