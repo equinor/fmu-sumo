f0837aeaf41088b3009ba0879b6fb21d951869c9
Raymond Wiker <rayw@equinor.com>
2024-09-13 09:42:08 +0200

    Remove tests for AggregatedTable, which does not exist anymore.

--- tests/test_objects_table.py
+++ tests/test_objects_table.py
@@ -7,7 +7,7 @@
 """
 import pandas as pd
 import pyarrow as pa
-from fmu.sumo.explorer import Explorer, AggregatedTable
+from fmu.sumo.explorer import Explorer
 import pytest
 
 # Fixed test case ("Drogon_AHM_2023-02-22") in Sumo/DEV
@@ -40,32 +40,3 @@ def test_table_to_arrow(table):
     arrow = table.to_arrow()
     assert isinstance(arrow, pa.Table)
 
-
-### Aggregated Table
-
-def test_aggregated_summary_arrow(case):
-    """Test usage of Aggregated class with default type"""
-
-    table = AggregatedTable(case, "summary", "eclipse", "iter-0")
-
-    assert len(table.columns) == 972 + 2
-    column = table["FOPT"]
-
-    assert isinstance(column.to_arrow(), pa.Table)
-    with pytest.raises(IndexError) as e_info:
-        table = table["banana"]
-        assert (
-            e_info.value.args[0] == "Column: 'banana' does not exist try again"
-        )
-
-
-def test_aggregated_summary_pandas(case):
-    """Test usage of Aggregated class with item_type=pandas"""
-    table = AggregatedTable(case, "summary", "eclipse", "iter-0")
-    assert isinstance(table["FOPT"].to_pandas(), pd.DataFrame)
-
-
-def test_get_fmu_iteration_parameters(case):
-    """Test getting the metadata of of an object"""
-    table = AggregatedTable(case, "summary", "eclipse", "iter-0")
-    assert isinstance(table.parameters, dict)
