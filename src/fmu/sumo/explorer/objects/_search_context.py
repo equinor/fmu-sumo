ffcbd1ad20c4febc972452165fd8cb491257c835
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2025-02-12 14:07:55 +0100

    iteration

--- src/fmu/sumo/explorer/objects/_search_context.py
+++ src/fmu/sumo/explorer/objects/_search_context.py
@@ -1104,7 +1104,9 @@ class SearchContext:
         Returns: iteration object
         """
         res = (
-            await self._sumo.post("/search", json=self._iteration_query(uuid))
+            await self._sumo.post_async(
+                "/search", json=self._iteration_query(uuid)
+            )
         ).json()
         obj = res["hits"]["hits"][0]
         obj["_id"] = uuid
