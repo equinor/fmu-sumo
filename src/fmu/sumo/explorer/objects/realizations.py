2782fab59f0642e8399da781f143d08cdb867a6e
Raymond Wiker <rayw@equinor.com>
2025-08-18 15:00:32 +0200

    feat: reference realizations (#438)
    * feat: implement functionality for reference realizations.
    
    * Added unit tests for reference realizations.
    
    * fix: refactor get_object and friends, to eliminate  infinite recursion bug.

--- src/fmu/sumo/explorer/objects/realizations.py
+++ src/fmu/sumo/explorer/objects/realizations.py
@@ -1,6 +1,6 @@
 """Module for searchcontext for collection of realizations."""
 
-from typing import Dict
+from typing import List
 
 from ._search_context import SearchContext
 
@@ -17,43 +17,26 @@ class Realizations(SearchContext):
     async def _maybe_prefetch_async(self, index):
         return
 
-    def get_object(self, uuid: str) -> Dict:
-        """Get metadata object by uuid
-
-        Args:
-            uuid (str): uuid of metadata object
-            select (List[str]): list of metadata fields to return
-
-        Returns:
-            Dict: a metadata object
-        """
-        obj = self._cache.get(uuid)
-        if obj is None:
-            obj = self.get_realization_by_uuid(uuid)
-            self._cache.put(uuid, obj)
-            pass
-
-        return obj
-
-    async def get_object_async(self, uuid: str) -> Dict:
-        """Get metadata object by uuid
-
-        Args:
-            uuid (str): uuid of metadata object
-            select (List[str]): list of metadata fields to return
-
-        Returns:
-            Dict: a metadata object
-        """
-
-        obj = self._cache.get(uuid)
-        if obj is None:
-            obj = await self.get_realization_by_uuid_async(uuid)
-            self._cache.put(uuid, obj)
-
-        return obj
-
     def filter(self, **kwargs):
         sc = super().filter(**kwargs)
         uuids = sc.get_field_values("fmu.realization.uuid.keyword")
         return Realizations(self, uuids)
+
+    @property
+    def classes(self) -> List[str]:
+        return ["realization"]
+
+    @property
+    async def classes_async(self) -> List[str]:
+        return ["realization"]
+
+    @property
+    def realizationids(self) -> List[int]:
+        return [self.get_object(uuid).realizationid for uuid in self._hits]
+
+    @property
+    async def realizationids_async(self) -> List[int]:
+        return [
+            (await self.get_object_async(uuid)).realizationid
+            for uuid in self._hits
+        ]
