f63565cc02befdb22b891f2a1c419932cd0161aa
Raymond Wiker <rayw@equinor.com>
2026-04-27 13:06:41 +0200

    chore: remove debug print.

--- tests/test_explorer.py
+++ tests/test_explorer.py
@@ -437,14 +437,11 @@ def test_buckets_partitioned(explorer: Explorer):
         .limit(10)
     )
     for ens in ensembles:
-        print(ens)
         tables = ens.tables.filter(tagname="summary", realization=True)
         res_p = tables._get_buckets_partitioned("data.spec.columns.keyword")
         res_c = tables._get_buckets("data.spec.columns.keyword")
         col_p = {b[0] for b in res_p}
         col_c = {b[0] for b in res_c}
-        print(f"len(col_p) = {len(col_p)}")
-        print(f"len(col_c) = {len(col_c)}")
 
         diff = col_p.symmetric_difference(col_c)
         assert len(diff) == 0
